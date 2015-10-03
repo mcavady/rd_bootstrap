e94875d390ca3b97a6c3c106dd2279f1692c66fe
james <git.demos>
2015-10-03 22:27:09 +0100

    refactor

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Responsive Developer - Websites - Bootstrap - Sass - Compass - JQuery - Ruby - html5</title>
+    <link rel="stylesheet" href="../rd_bootstrap/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../rd_bootstrap/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
+    <script src="../rd_bootstrap/js/jquery-1.11.3.min.js"></script>
+    <script src="../rd_bootstrap/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+    <link href="../rd_bootstrap/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="../rd_bootstrap/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+    <!--[if IE]>
+      <link href="../rd_bootstrap/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <![endif]-->
+  </head>
+    <body>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+       </nav>
+       <div class="container">
+       <header>
+         <div id="header">
+           header
+         </div><!-- end the header -->
+       </header>
+       <div id="content-wrapper">
+       <aside>
+        <div id="sidebar">
+    		sidebar
+    	</div><!-- end the side bar -->
+       </aside>
+       <div id="main-content">
+       	main content
+       </div>
+       <footer>
+         <div id="footer">
+              	footer
+         </div>
+       </footer>
+        </div><!--end the container -->
+    </body>
+</html>
